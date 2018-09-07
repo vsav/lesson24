57004b691f96de55972610746e16f7ed2a9e8ced
Vadim <vsavdev@gmail.com>
2018-09-07 06:05:42 +0300

    Added error messages

--- app.rb
+++ app.rb
@@ -27,19 +27,21 @@ post '/visit' do
 	@phone = params[:phone]
 	@date = params[:datetime]
   @master = params[:master]
+
   if @name != "" && @phone != "" && @date != ""
-	  @ok_message = "Thank you, #{@name}! We'll be waiting for you at #{@date}"
+
+    @ok_message = "Thank you, #{@name}! We'll be waiting for you at #{@date}"
 
     f = File.open('./public/visitors.txt', 'a')
     f.write "Name: #{@name}, Phone: #{@phone}, Visit date: #{@date}, Master: #{@master} \n"
     f.close
 
   elsif @name == ""
-    @name_error_message = "Please enter your name"
+    @error = "Please enter your name"
   elsif @phone == ""
-    @phone_error_message = "Please enter your phone number"
+    @error = "Please enter your phone number"
   elsif @date == ""
-    @date_error_message = "Please choose visit date"
+    @error = "Please choose visit date"
   end
 
   erb :visit
